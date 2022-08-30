39f8d1d7f766ce64603b0a5fd5ee6cde44786591
FloBB22000 <florence.goslino@sncf.fr>
2022-08-30 15:48:26 +0200

    quete 5 express terminée

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", usersHandlers.postUsers);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", usersHandlers.deleteUsers);
 app.put("/api/users/:id", usersHandlers.updateUsers);
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
