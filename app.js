0d5036630652c5c16e2906ca2fd7eaf685399d50
FloBB22000 <florence.goslino@sncf.fr>
2022-09-20 11:27:56 +0200

    express 8 terminé

--- app.js
+++ app.js
@@ -12,25 +12,31 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
+
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 const usersHandlers = require("./usersHandlers");
 const { validateMovie }= require("./validators.js");
 const { validateUser }= require("./validators.js");
-const { hashedPassword } = require("./auth.js");
-
+const { hashedPassword, verifyPassword, verifyToken } = require("./auth.js");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-app.post("/api/users", validateUser,hashedPassword, usersHandlers.postUsers);
-app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-app.delete("/api/users/:id", usersHandlers.deleteUsers);
-app.put("/api/users/:id", validateUser, usersHandlers.updateUsers);
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
+app.post("/api/users", validateUser,hashedPassword, usersHandlers.postUsers);
+app.post("/api/login", usersHandlers.getUserByEmailWithPasswordAndPassToNext,verifyPassword);
+
+app.use(verifyToken);
+app.post("/api/movies", verifyToken, validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.put("/api/users/:id", validateUser, usersHandlers.updateUsers);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", usersHandlers.deleteUsers);
+
+
+
 
 app.listen(port, (err) => {
   if (err) {
@@ -39,3 +45,15 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+const isItDwight = (req, res) => {
+  if (req.body.email === "dwight@theoffice.com" && req.body.password === "123456") {
+    res.send("Credentials are valid");
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+app.post("/api/login", isItDwight);
+
+
